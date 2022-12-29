cba4ec65354c734bb1ea76627b4c068087d7656a
Roeegg2 <86188707+Roeegg2@users.noreply.github.com>
2022-12-29 17:47:06 +0200

    Add files via upload

---
+++ cameraTask/Activity1.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cameratask
+{
+	[Activity(Label = "Activity1")]
+	public class Activity1 : Activity
+	{
+		TextView tv;
+
+		string[] array1 = new string[3];
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			SetContentView(Resource.Layout.layout1);
+
+			array1[0] = Intent.GetStringExtra("fname");
+			array1[1] = Intent.GetStringExtra("lname");
+			array1[2] = Intent.GetStringExtra("address");
+
+			tv.Text = "its cone" + array1[0] + array1[1] + array1[2];
+		}
+	}
+}
---
+++ cameraTask/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Views;
+using Android.Widget;
+using Android.Content;
+
+namespace cameratask
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, Android.Views.View.IOnClickListener
+	{
+        Button submitBtn;
+        EditText fname;
+        EditText lname;
+        EditText address;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+           
+
+            SetContentView(Resource.Layout.activity_main);
+
+            submitBtn = FindViewById<Button>(Resource.Id.submitbtn);
+            submitBtn.SetOnClickListener(this);
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+		public void OnClick(View v)
+		{
+			if (submitBtn == v)
+			{
+                Intent intent = new Intent(this, typeof(Activity1));
+                intent.PutExtra("fname", fname.Text);
+                intent.PutExtra("lname", lname.Text);
+                intent.PutExtra("address", address.Text);
+                StartActivity(intent);
+			}
+		}
+
+    }
+}
